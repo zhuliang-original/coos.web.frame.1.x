9d73576e93b8084a46d4ec7ad41a49b851aefaa8
coosx <zhuliang@coos.top>
2018-04-02 18:19:25 +0800

    优化

--- src/main/webapp/resource/dist/js/base/plugin.js
+++ src/main/webapp/resource/dist/js/base/plugin.js
@@ -30,8 +30,7 @@
 			js : [ "resource/plugins/bootstrap/switch/bootstrap-switch.min.js" ],
 			css : [ "resource/plugins/bootstrap/switch/bootstrap-switch.css" ]
 		},
-		data_tables :
-		{
+		data_tables : {
 			js : [ "resource/plugins/jquery/datatables/jquery.datatables.min.js", "resource/plugins/jquery/datatables/datatables.bootstrap.js" ],
 			css : [ "resource/plugins/jquery/datatables/css/datatables.bootstrap.css", "resource/plugins/jquery/datatables/css/jquery.datatables.css" ]
 		},
@@ -90,16 +89,20 @@
 			css : []
 		},
 		chartist : {
-			js : [ "resource/plugins/chartist/chartist.min.js"],
+			js : [ "resource/plugins/chartist/chartist.min.js" ],
 			css : [ "resource/plugins/chartist/chartist.min.css" ]
 		},
 		loaders : {
-			js : [ ],
+			js : [],
 			css : [ "resource/plugins/loaders/loaders.min.css" ]
 		},
 		flot : {
-			js : [ "resource/plugins/flot/jquery.flot.js"],
-			css : [ ]
+			js : [ "resource/plugins/flot/jquery.flot.js" ],
+			css : []
+		},
+		flot_pie : {
+			js : [ "resource/plugins/flot/jquery.flot.js", "resource/plugins/flot/jquery.flot.pie.js" ],
+			css : []
 		}
 	};
 
