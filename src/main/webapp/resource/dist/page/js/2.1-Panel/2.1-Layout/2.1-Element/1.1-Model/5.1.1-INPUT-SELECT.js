83c8b893c90f9afe04e8826ba35159b25f550a7b
coosx <zhuliang@coos.top>
2018-06-05 13:46:50 +0800

    添加输入下拉框支持

---
+++ src/main/webapp/resource/dist/page/js/2.1-Panel/2.1-Layout/2.1-Element/1.1-Model/5.1.1-INPUT-SELECT.js
@@ -0,0 +1,30 @@
+(function(window, jQuery) {
+	function ThisElement(config) {
+		co.page.panel.layout.element.Element.call(this, config);
+	}
+	(function() {
+		var Super = function() {
+		};
+		Super.prototype = co.page.panel.layout.element.Element.prototype;
+		ThisElement.prototype = new Super();
+	})();
+
+	ThisElement.prototype.textUseSelectData = function() {
+		return true;
+	};
+
+	ThisElement.prototype.initInput = function($input) {
+		$input.addClass('input-rule-select');
+
+	};
+
+	var ThisElementConfig = {
+		name : "输入下拉框",
+		forInput : true,
+		columns : [ {
+			text : "级联元素",
+			name : "relationname"
+		} ]
+	};
+	co.page.panel.layout.element.model.defind("INPUT-SELECT", ThisElementConfig, ThisElement);
+})(window, jQuery);
